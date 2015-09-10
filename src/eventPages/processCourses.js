3f96f94b1b5ee13344ce6d0f11b170652efa40ee
Andrew DiPrinzio <ajd416@lehigh.edu>
2015-09-10 00:51:59 -0400

    refactored, added GA support

--- src/eventPages/processCourses.js
+++ src/eventPages/processCourses.js
@@ -1,23 +1,13 @@
-chrome.runtime.onInstalled.addListener(function() {
-  // Replace all rules ...
-  chrome.declarativeContent.onPageChanged.removeRules(undefined, function() {
-    // With a new rule ...
-    chrome.declarativeContent.onPageChanged.addRules([
-      {
-        conditions: [
-          new chrome.declarativeContent.PageStateMatcher({
-            pageUrl: { urlContains: 'bwskfshd.P_CrseSchdDetl' },
-          })
-        ],
-        // And shows the extension's page action.
-        actions: [ new chrome.declarativeContent.ShowPageAction(), ]
-      }
-    ]);
-  });
-});
+(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+})(window,document,'script','https://www.google-analytics.com/analytics.js','ga'); // Note: https protocol here
 
-chrome.runtime.onMessage.addListener(function(data,sender,sendResponse){
-  sendResponse();
+ga('create', 'UA-26151492-2', 'auto');
+ga('set', 'checkProtocolTask', null);
+
+
+chrome.runtime.onMessage.addListener(function(data,sender){
 
   chrome.identity.getAuthToken({ 'interactive': true }, function(token) {
 
@@ -46,8 +36,8 @@ chrome.runtime.onMessage.addListener(function(data,sender,sendResponse){
           iconUrl: "icons/icon-48.png"
         }
         chrome.notifications.create(opt);
+        ga('send', 'event', 'process', 'courses');
       }
-
     };
 
     xhr.open("POST", "https://www.googleapis.com/calendar/v3/calendars/", true);
